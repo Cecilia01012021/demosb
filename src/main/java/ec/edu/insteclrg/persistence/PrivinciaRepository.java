80f22d926363c48150a5dff2ee88f4f728cd8081
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-06-01 08:52:12 -0500

    initial

---
+++ src/main/java/ec/edu/insteclrg/persistence/PrivinciaRepository.java
@@ -0,0 +1,9 @@
+package ec.edu.insteclrg.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.insteclrg.domain.Provincia;
+
+public interface PrivinciaRepository  extends  JpaRepository < Provincia, Long>{
+
+}
---
+++ src/main/java/ec/edu/insteclrg/service/crud/ProvinciaService.java
@@ -0,0 +1,29 @@
+package ec.edu.insteclrg.service.crud;
+
+import java.util.Optional;
+
+import ec.edu.insteclrg.domain.Provincia;
+import ec.edu.insteclrg.dto.ProvinciaDTO;
+import ec.edu.insteclrg.service.GenericCrudServiceImpl;
+
+public class ProvinciaService extends GenericCrudServiceImpl<Provincia, ProvinciaDTO>{
+
+	@Override
+	public Optional<Provincia> find(ProvinciaDTO dto) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public ProvinciaDTO mapToDto(Provincia domain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Provincia mapToDomain(ProvinciaDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
